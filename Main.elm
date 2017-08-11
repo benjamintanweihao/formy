25da04e70af1a35bb56618d14697a110aec9ff44
Benjamin Tan Wei Hao <benjamintanweihao@gmail.com>
2017-08-12 01:42:03 +0800

    Added Elm decoders and mock JSON data

--- Main.elm
+++ Main.elm
@@ -2,6 +2,8 @@ module Main exposing (..)
 
 import Html exposing (..)
 import Html.Attributes exposing (..)
+import Json.Decode as Decode exposing (..)
+import Http exposing (..)
 
 
 main =
@@ -14,12 +16,15 @@ main =
 
 type alias Model = {}
 
+
 model : Model
 model = {}
 
 -- UPDATE
 
+
 type Msg = Foo
+
 
 update : Msg -> Model -> Model
 update msg model =
@@ -28,6 +33,7 @@ update msg model =
 
 -- VIEW
 
+
 view : Model -> Html Msg
 view model =
     div []
@@ -35,5 +41,33 @@ view model =
         , input [ type_ "text", placeholder "Image Version", disabled True] []
         ]
 
-    -- [ input [ type_ "text", placeholder "Name", onInput Name ] []
+type alias ImageTag = String
 
+
+type alias ImageDetail =
+    { repositoryName : String
+    , imageTags : List ImageTag
+    }
+
+
+imageDetailListDecoder : Decoder (List ImageDetail)
+imageDetailListDecoder =
+    Decode.list imageDetailDecoder
+
+
+imageDetailDecoder : Decode.Decoder ImageDetail
+imageDetailDecoder =
+    Decode.map2
+      ImageDetail
+      (Decode.at [ "repositoryName" ] Decode.string)
+      (Decode.at [ "imageTags" ] (Decode.list Decode.string))
+
+
+url : String
+url =
+    "https://my-json-server.typicode.com/benjamintanweihao/formy/imageDetails"
+
+
+getImageDetails : Http.Request (List ImageDetail)
+getImageDetails =
+    Http.get url imageDetailListDecoder
